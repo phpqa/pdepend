ca35b8be17e7a13b49e8fddad48f7427eb693d0a
MetalArend <metalarend@gmail.com>
2018-06-28 00:09:23 +0200

    enhance the tests and variables used

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/usr/bin/env sh
 set -e
 
-if [ "${1:0:1}" = "-" ]; then
-  set -- /sbin/tini -- php /vendor/bin/pdepend "$@"
-elif [ "$1" = "/vendor/bin/pdepend" ]; then
+if [ "$(printf %c "$1")" = '-' ]; then
+  set -- /sbin/tini -- php /composer/vendor/bin/pdepend "$@"
+elif [ "$1" = "/composer/vendor/bin/pdepend" ]; then
   set -- /sbin/tini -- php "$@"
 elif [ "$1" = "pdepend" ]; then
-  set -- /sbin/tini -- php /vendor/bin/"$@"
+  set -- /sbin/tini -- php /composer/vendor/bin/"$@"
 elif [ -d "$1" ]; then
-  set -- /sbin/tini -- php /vendor/bin/pdepend "$@"
+  set -- /sbin/tini -- php /composer/vendor/bin/pdepend "$@"
 fi
 
 exec "$@"
